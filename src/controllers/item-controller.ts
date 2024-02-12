6023feb2e18b17a8c51d5152f8fd1d6dc3bae97e
yasindu <yazindu@gmail.com>
2024-02-12 15:28:15 +0530

    wip: Write CRUD operations

--- src/controllers/item-controller.ts
+++ src/controllers/item-controller.ts
@@ -5,9 +5,9 @@ import {v4 as uuid} from 'uuid'
 export class ItemController {
     public static async createItem(req: Request, res: Response) {
         const {name} = req.body
-        if(!name) return res.status(400).json({message: 'Name is required!'})
+        if (!name) return res.status(400).json({message: 'Name is required!'})
 
-        try{
+        try {
             const newItem = new ItemsSchemaModel({
                 name: name,
                 id: uuid(),
@@ -17,16 +17,40 @@ export class ItemController {
 
             // const result  = await itemsCollection.insertOne({name, id: uuid()})
             res.status(200).json({message: 'Item added successfully', result})
-        }catch(ex){
+        } catch (ex) {
             res.status(400).json({message: 'Error occurred'})
         }
     }
 
     public static async getAllItems(req: Request, res: Response) {
-        try{
+        try {
             const result = await ItemsSchemaModel.find()
             res.status(200).json({message: 'Get all items successfully', result})
-        }catch(ex){
+        } catch (ex) {
+            res.status(400).json({message: 'Error occurred'})
+        }
+    }
+
+    public static async getItem(req: Request, res: Response) {
+        const {name} = req.params;
+        try {
+            const result = await ItemsSchemaModel.findOne({name: name})
+            if (result) res.status(200).json({message: 'Get item successful', result})
+            else res.status(200).json({message: 'Item not found'})
+        } catch (ex) {
+            res.status(400).json({message: 'Error occurred'})
+        }
+    }
+
+    public static async updateItem(req: Request, res: Response) {
+        const {name} = req.params;
+        const {newName} = req.body
+        try {
+            const result = await ItemsSchemaModel.findOneAndUpdate(
+                {name: name}, {name: newName}
+            )
+            res.status(200).json({message: 'Update item successful', result})
+        } catch (ex) {
             res.status(400).json({message: 'Error occurred'})
         }
     }
--- src/routes/item-routes.ts
+++ src/routes/item-routes.ts
@@ -4,6 +4,8 @@ import {ItemController} from "../controllers/item-controller";
 const router = express.Router()
 
 router.post('/add-item', ItemController.createItem);
-router.get('/', ItemController.getAllItems);
+router.get('/get-item/:name', ItemController.getItem);
+router.get('/get-items', ItemController.getAllItems);
+router.put('/update-item/:name', ItemController.updateItem);
 
 export default router
